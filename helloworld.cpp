b3722cb5cbee59c1bf9e4de5f3412e91bff5da5a
root <root@localhost.localdomain>
2019-07-26 10:04:38 +0900

    helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+#include <eosio/eosio.hpp>
+
+
+using namespace eosio;
+
+
+CONTRACT helloworld: public contract {
+
+public:
+
+using contract::contract;
+
+
+ACTION hi(name user) {
+
+require_auth(user);
+
+
+print("Hello, ", user);
+
+}
+
+};
